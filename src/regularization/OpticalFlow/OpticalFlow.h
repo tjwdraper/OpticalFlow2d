a95943b4924a415af275c8293c2004c3b0a45652
Tom Draper <tjwdraper@gmail.com>
2026-09-11 01:49:08 +0200

    Removed the OpticalFlowDiffusion class. In current implementation, there is only one OpticalFlow class, Diffusion subclass was redundant.

--- src/regularization/OpticalFlow/OpticalFlow.h
+++ src/regularization/OpticalFlow/OpticalFlow.h
@@ -9,18 +9,30 @@
 class OpticalFlow : public IterativeSolver {
     public:
         // Constructors and deconstructors
-        OpticalFlow(const dim dimin);
+        OpticalFlow(const dim dimin, const double alpha);
         ~OpticalFlow();
 
         // Construct the force from the image gradients and motion estimate
         void get_force(Motion* force, const Motion* motion) const;
 
         // Do one iteration
-        virtual void get_update(Motion *motion, const Image* Iref = NULL, const Image* Imov = NULL) {};
+        void get_update(Motion *motion, const Image* Iref = NULL, const Image* Imov = NULL) {};
 
     protected:
         // Spatial and temporal image gradients
-        Motion *force;
+        Motion *force;  
+        
+        // Do one iteration of the Horn-Schunck method (= Optical Flow Diffusion)
+        void optical_flow_iteration(Motion* motion);
+
+        // Get the FD approximation of the motion, wwithout the "central" contribution
+        void get_quasi_differential_operator(const Motion* motion);
+
+        // Quasi differential operator
+        Motion *qdiffoperator;
+
+        // Regularisation parameters
+        double alpha;
 };
 
 #endif
